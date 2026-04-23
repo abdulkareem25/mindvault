d8faffee22c582df3c0a1a2e84fe29c2aaf13232
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2026-04-23 09:56:34 +0530

    cors used to connect frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 import './config/env.js';
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import notFoundMiddleware from './middlewares/notFound.middleware.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import authRouter from './routes/auth.routes.js';
@@ -10,6 +11,12 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use('/api/auth', authRouter);
 app.use('/api/problems', problemRouter);
