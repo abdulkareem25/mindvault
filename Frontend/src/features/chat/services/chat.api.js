0afdfa5c49d8680255e7780bd2727b04843ee11e
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2026-04-30 19:46:54 +0530

    Fix baseURL configuration in auth and chat API services for production deployment

--- Frontend/src/features/chat/services/chat.api.js
+++ Frontend/src/features/chat/services/chat.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/chats",
+  baseURL: "http://localhost:3000/api/chats" || "https://mindvault-6exy.onrender.com/api/chats",
   withCredentials: true
 });
 
