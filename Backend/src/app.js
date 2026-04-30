4da01c58b8b11bc7f2a40d262b2a4288ac47845d
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2026-04-30 19:37:13 +0530

    Add initial HTML and JavaScript assets for MindVault application

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -18,6 +18,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
+app.use(express.static('./public'));
 
 app.use('/api/auth', authRouter);
 app.use('/api/chats', chatRouter);
